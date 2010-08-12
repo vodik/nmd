a29b82dd294f8a0504a38a538b224e2a8440578b
simongmzlj <simongmzlj@gmail.com>
2010-08-12 03:04:19 -0400

    forgot to add nmd.c

---
+++ nmd.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "interfaces.h"
+
+int
+main(int argc, char *argv[])
+{
+	const struct interface *ife;
+	for (ife = get_interfaces(); ife; ife = ife->next)
+		printf("found dev %s\n", ife->name);
+	for (ife = get_interfaces(); ife; ife = ife->next)
+		printf("found dev %s\n", ife->name);
+	return 0;
+}
